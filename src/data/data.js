94202ac8a9e1975d8a209f2ed308a2813a88a3f2
YooZiiX <jeremy.lucas@mines-ales.org>
2023-11-29 18:56:05 +0100

    implémentation des Tags de couleur personnalisé, càd de la forme hexa #......

--- src/data/data.js
+++ src/data/data.js
@@ -3,14 +3,14 @@ export const memes = [
         _id: "11",
         tags: [
             {
-                _id: "2",
+                _id: "6",
                 label: "Comédie",
-                color: "red",
+                color: "#4ECDC4",
             },
             {
                 _id: "18",
                 label: "Architecture",
-                color: "cyan",
+                color: "#FF6B6B",
             },
         ],
         img: "a",
@@ -19,9 +19,9 @@ export const memes = [
         _id: "2",
         tags: [
             {
-                _id: "2",
+                _id: "21",
                 label: "Nourriture",
-                color: "green",
+                color: "#E43F6F",
             },
         ],
         img: "a",
@@ -30,19 +30,19 @@ export const memes = [
         _id: "3",
         tags: [
             {
-                _id: "2",
+                _id: "24",
                 label: "Drapeau",
-                color: "blue",
+                color: "#FFC857",
             },
             {
-                _id: "18",
+                _id: "12",
                 label: "Physique",
-                color: "cyan",
+                color: "#119DA4",
             },
             {
-                _id: "18",
+                _id: "10",
                 label: "Souris",
-                color: "orange",
+                color: "#F28F3B",
             },
         ],
         img: "a",
