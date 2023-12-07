f7d7e1d8a9a38cb0c4775705c532aa98cf832832
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-07 14:42:40 +0100

    ajout du Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Button from "../components/ui/Button";
+import Header from "../components/Header";
+// import Container from "../components/ui/Container";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='w-screen h-screen'>
+      <Header />
       <Button title="DEV" to="/dev"/>
     </div>
   )
