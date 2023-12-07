7c6ce9f1020686dbb7564ce71bdec4fab85f3d1c
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-07 12:41:04 +0100

    Ajout d'un composant Display pour afficher les memes

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Container from '../components/ui/Container';
 import Header from "../components/Header";
 import Display from '../components/ui/Display';
-import Tag from '../components/ui/Tag';
 import {memes} from "../data/data";
 
-const HomePage = () => {
+const TestPage = () => {
   return (
     <div className='w-screen'>
       <Header />
       <Container>
-          <div className="grid grid-cols-4 gap-3">
+          <div className="grid grid-cols-4 gap-x-3 gap-y-2">
             {
-              memes.map((meme) => (
-                <div key={meme._id}>
-                  <Link to={`/meme/${meme._id}`} >
-                    <Display />
-                  </Link>
-                  <div className='flex justify-center'>
-                  {
-                  meme.tags.map((tag)=>(
-                    <div key={tag._id} className='my-1 mx-1'>
-                      <Tag title={tag.label} to="/" color={tag.color}/>
-                    </div>
-                  ))
-                  }
-                  </div>
-                </div>
-              ))
+            memes.map((meme) => (
+              <div key={meme._id} className="flex justify-center">
+                <Display to={`/meme/${meme._id}`} tags={meme.tags} />
+              </div>
+            ))
             }
           </div>
       </Container>
@@ -36,4 +23,4 @@ const HomePage = () => {
   )
 }
 
-export default HomePage;
+export default TestPage;
