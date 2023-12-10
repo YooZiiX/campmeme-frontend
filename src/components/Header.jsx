d93ccb99bf03075f88714f889d3a9bf8a526c7d2
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-10 18:46:48 +0100

    Modification des chemins Connexion & Inscription

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="z-50 top-0 sticky w-full bg-white shadow">
       <nav className="bg-white max-w-6xl mx-auto flex items-center justify-between">
-        <div className='absolute left-2 bg-gray-400 w-11 h-11 rounded-md' onClick={() => console.log("clicked")}>
+        <div className='absolute left-2 bg-gray-400 w-11 h-11 rounded-md'>
           .
         </div>
         <Link to="/" className="flex m-1">
@@ -15,8 +15,8 @@ const Header = () => {
         </Link>
         <div className='bg-red-400 w-96 p-1 rounded-md'>Barre de recherche</div>
         <div className="flex justify-center">
-          <Button title="inscription" to="/"/>
-          <Button title="connexion" to="/dev"/>
+          <Button title="inscription" to="/inscription"/>
+          <Button title="connexion" to="/connexion"/>
           <Link to="/" className={`bg-red-400 mt-2 rounded-full w-11 h-11 text-center`}>
             <img src={ProfilPicture} alt="profil" className='bg-cover rounded-full w-full h-full'/>
           </Link>
