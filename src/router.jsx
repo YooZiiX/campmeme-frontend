7b0b8a67ea57a5c00babaeeff7393f051382a995
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-14 09:04:39 +0100

    Refactoring HomePage

--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,7 @@ import MemePage from './pages/MemePage';
 import TestPage from './pages/TestPage';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
+import Upload from './pages/Upload';
 
 function Router() {
   return (
@@ -14,6 +15,7 @@ function Router() {
             <Route path='/meme/:id' element={<MemePage />} />
             <Route path='/connexion' element={<SigninPage />} />
             <Route path='/inscription' element={<SignupPage />} />
+            <Route path='/upload' element={<Upload />} />
             <Route path='/dev' element={<TestPage />} />
         </Routes>
     </BrowserRouter>
