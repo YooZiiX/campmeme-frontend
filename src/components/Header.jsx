3d0fea70b2ace99f1d427fe437a80f0fe51aff11
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-15 00:30:14 +0100

    Modification style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,18 @@ import logo from "../assets/logo.png"
 const Header = () => {
   const userConnected = window.localStorage.getItem("userId");
   return (
-    <header className="z-50 top-0 sticky w-full bg-[FDFDFD]  border-b-2 border-neutral-200 shadow">
-      <nav className="bg-white max-w-6xl mx-auto flex items-center justify-between p-1">
+    <header className="z-50 top-0 sticky w-full bg-[#FDFDFD]  border-b-2 border-neutral-200 shadow">
+      <nav className="bg-[#FDFDFD] max-w-6xl mx-auto flex items-center justify-between p-1">
         <Link to="/" className="flex m-1 items-center">
+          <img src={logo} alt='icon' className='m-2 mr-30 w-12 h-10' />
           <img src={campmeme} alt='icon' className='m-2 mr-50 w-23 h-7' /> 
-          <img src={logo} alt='icon' className='m-2 mr-30 w-12 h-13' />
         </Link>
         <div className='bg-[#FFFFFF] w-96 ml-40 mr-10 p-1 rounded-md border-2'>Barre de recherche</div>
         <div className="flex justify-center items-center">
           {
             userConnected ? (
               <>
-                <Link to="/" className={`bg-red-400 rounded-full w-10 h-10 text-center`}>
+                <Link to="/" className={`bg-red-400 rounded-full w-12 h-12 text-center`}>
                   <img src={ProfilPicture} alt="profil" className='bg-cover rounded-full w-full h-full' />
                 </Link>
               </>
