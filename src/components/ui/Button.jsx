0a0892e1d17e2bbfecc4bb1bb1f516958cceb73b
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-14 13:55:15 +0100

    Modification du style du Composant Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Button = (props) => {
   return (
-    <Link to={props.to} style={{background: `${props.bg}`}} className={`flex rounded-md w-fit m-2 px-2 py-1 text-black text-center text-lg font-bold uppercase ${props.className}`}>
-        {props.title}
+    <Link to={props.to} style={{background: `${props.bg}`}} className={`flex rounded-md w-fit m-2 px-2 py-1 text-black text-center text-lg font-bold uppercase ${props.className} hover:shadow-md duration-100`}>
+        <p className='text-black'>
+          {props.title}
+        </p>
     </Link>
     
   )
